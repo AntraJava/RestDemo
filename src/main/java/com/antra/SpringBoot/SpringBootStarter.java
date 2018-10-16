cfec189ff91b8289883c422d335479e91f713a89
antrajava <antrajava>
2018-10-16 12:09:20 -0400

    Rest example

---
+++ src/main/java/com/antra/SpringBoot/SpringBootStarter.java
@@ -0,0 +1,13 @@
+package com.antra.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages= {"com.antra"})
+public class SpringBootStarter {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootStarter.class, args);
+	}
+}
---
+++ src/main/java/com/antra/model/User.java
@@ -0,0 +1,57 @@
+package com.antra.model;
+
+public class User {
+
+	private long id;
+    
+    private String name;
+     
+    private int age;
+     
+    private double salary;
+
+    public User() {
+    	id=0;
+    }
+	public User(long id, String name, int age, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+    
+    
+}

a4a5861524a5e695991f3f691c382b19c0f03844
antrajava <antrajava>
2018-10-30 13:17:52 -0400

    new code

--- SpringRest/src/test/java/com/antra/testClient/SpringBootRestTestClient.java
+++ SpringRest/src/test/java/com/antra/testClient/SpringBootRestTestClient.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
